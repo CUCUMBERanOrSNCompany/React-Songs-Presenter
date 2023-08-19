2562ab8353c4d0d73a5650374eea86cf9851fe0d
CUCUMBERanOrSNCompany <orsn349@yahoo.com>
2023-08-19 11:43:17 +0300

    0.7.1
    Completed Frontend functionality

--- Frontend/view/src/App.jsx
+++ Frontend/view/src/App.jsx
@@ -1,46 +1,48 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-    const [songs, setSongs] = useState([]);
+  const [songs, setSongs] = useState([]);
 
-    console.log('App component rendered');
+  console.log('App component rendered');
 
-    useEffect(() => {
-        fetch('/api/songs')
-            .then(response => {
-                console.log('Raw response:', response);
-                return response.json();
-            })
-            .then(data => {
-                console.log('Parsed data:', data);
-                setSongs(data);
-            })
-            .catch(error => console.error('Error fetching songs:', error));
-    }, []);
+  useEffect(() => {
+    console.log('Fetching songs...');
+    fetch('/api/songs')
+        .then(response => {
+          console.log('Raw response:', response);
+          return response.json();
+        })
+        .then(data => {
+          console.log('Parsed data:', data);
+          setSongs(data);
+        })
+        .catch(error => console.error('Error fetching songs:', error));
+  }, []);
 
-    return (
-        <div>
-            <h1>Songs List</h1>
-            <table>
-                <thead>
-                <tr>
-                    <th>Song Name</th>
-                    <th>Band</th>
-                    <th>Year</th>
-                </tr>
-                </thead>
-                <tbody>
-                {songs.map(song => (
-                    <tr key={song.id}>
-                        <td>{song.songName}</td>
-                        <td>{song.band}</td>
-                        <td>{song.year}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+      <>
+        <h1>Songs List</h1>
+        <h5>Sorted by the Band Name:</h5>
+        <table>
+          <thead>
+          <tr>
+            <th>Song Name</th>
+            <th>Band</th>
+            <th>Year</th>
+          </tr>
+          </thead>
+          <tbody>
+          {songs.map(song => (
+              <tr key={song.id}>
+                <td>{song.songName}</td>
+                <td>{song.band}</td>
+                <td>{song.year}</td>
+              </tr>
+          ))}
+          </tbody>
+        </table>
+      </>
+  );
 }
 
 export default App;
